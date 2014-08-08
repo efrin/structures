1f95027d818dd8cf4f3c40959b47447cbd681a38
ephrin <lazycommit@gmail.com>
2014-08-08 17:35:43 +0300

    Create ArrayStructure.php
    Method isSequental() provides some fast method to ensure that array has structure which encodes to Javascript array representation.
    Useful in data conversions like: mongodb stored values, json_encode result, etc.

---
+++ Ephrin/Structures/ArrayStructure.php
@@ -0,0 +1,52 @@
+<?php
+
+
+namespace Ephrin\Structures;
+
+
+/**
+ * Class ArrayStructure
+ * @package Ephrin\Glutton\MongoDB\Detector
+ */
+class ArrayStructure
+{
+    /**
+     * @param array|\ArrayAccess|\Countable $value
+     * @return bool
+     * @throws \InvalidArgumentException
+     */
+    public static function isSequential($value){
+        if(is_array($value) || ($value instanceof \Countable && $value instanceof \ArrayAccess)){
+            for ($i = count($value) - 1; $i >= 0; $i--) {
+                if (!isset($value[$i]) && !array_key_exists($i, $value)) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            throw new \InvalidArgumentException(
+                sprintf('Data type "%s" is not supported by method %s', gettype($value), __METHOD__)
+            );
+        }
+    }
+
+    /**
+     * Reverse to \Ephrin\Structures\ArrayStructure::isSequential
+     * @param $value
+     * @return bool
+     */
+    public static function isHash($value)
+    {
+        return !self::isSequential($value);
+    }
+
+    /**
+     * Another one 
+     * @param $value
+     * @return bool
+     */
+    public static function isAssoc($value)
+    {
+        return array_keys($value) !== range(0, count($value) - 1);
+    }
+} 
